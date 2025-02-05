bc7b768baa2a500c981df54afe501ecf006615ca
DiegoGuevara2811 <gue24128@uvg.edu.gt>
2025-02-05 11:35:06 -0600

    Update ISorter.java

--- ISorter.java
+++ ISorter.java
@@ -1,7 +1,16 @@
+/**
+ * Interfaz genérica para algoritmos de ordenamiento.
+ * Define un método sort que mide y devuelve el tiempo de ejecución en nanosegundos.
+ *
+ * @param <T> Tipo de datos a ordenar, debe implementar Comparable<T>.
+ */
+public interface ISorter<T extends Comparable<T>> {
 
-public interface ISorter extends Comparable<T> {
-
-    
+    /**
+     * Ordena el arreglo dado y devuelve el tiempo de ejecución en nanosegundos.
+     *
+     * @param array El arreglo a ordenar.
+     * @return Tiempo de ejecución en nanosegundos.
+     */
     long sort(T[] array);
-
 }
